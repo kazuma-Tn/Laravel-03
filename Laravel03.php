d38a93747f241d8ed9747c68c3c275f5044e1129
kazuma <cazuma11@yahoo.co.jp>
2021-03-27 00:39:11 +0000

    Laravel03課題

--- Laravel03.php
+++ Laravel03.php
@@ -25,7 +25,7 @@ foreach($fruits as $fruit){
 $start = 1;
 /* for文の終わりの値を定義する */
 $end = 100;
-for($i = $start; $i < $end; $i++){
+for($i = $start; $i <= $end; $i++){
   // 5で割り切れたら{}内を実行する
   if ($i % 5 == 0){
     echo $i;
